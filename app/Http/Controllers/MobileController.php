a1da2c653452d767e88054d0047462333a973c0e
Arshad Iqbal <jdoe99053@gmail.com>
2021-05-23 01:39:21 -0700

    asd

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -809,8 +809,8 @@ class MobileController extends Controller
     }
 
     public function hospital_list_nearby(Request $request){
-        $list = Hospital::select('*', DB::raw('SQRT( POW(69.1 * (latitude - '.$request->input('lat').'), 2) + POW(69.1 * ('.$request->input('lng').' - longitude) * 
-        COS(latitude / 57.3), 2)) AS distance'))
+        $list = Hospital::select('*', DB::raw('SQRT( POW(69.1 * (lat - '.$request->input('lat').'), 2) + POW(69.1 * ('.$request->input('lng').' - lng) * 
+        COS(lat / 57.3), 2)) AS distance'))
         ->having('distance', '<', 25)
         ->orderBy('distance')->get()->toArray();
 
