08303a6c3834df02c3febd9ce4c3ea8e206be3ea
Arshad Iqbal <jdoe99053@gmail.com>
2021-06-20 20:31:22 -0700

    asd

--- resources/views/layout/base/_topbar-right.blade.php
+++ resources/views/layout/base/_topbar-right.blade.php
@@ -4,8 +4,7 @@
     <div class="topbar-item">
         <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
             <div class="d-flex flex-column text-right pr-3">
-                <span class="text-muted font-weight-bold font-size-base d-none d-md-inline">{{Auth::user()->first_name}}</span>
-                <span class="text-dark-75 font-weight-bolder font-size-base d-none d-md-inline">{{Auth::user()->role}}</span>
+                <span class="text-muted font-weight-bold font-size-base d-none d-md-inline">{{Auth::user()->name}}</span>
             </div>
             <span class="symbol symbol-35 symbol-light-primary">
                 <span class="symbol-label font-size-h5 font-weight-bold">S</span>
