6a35976896a61615d67685dadb6af3fddef8c209
Arshad Iqbal <jdoe99053@gmail.com>
2021-06-26 08:29:25 -0700

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,10 +36,11 @@ class HomeController extends Controller
         if(Auth::user()->role == 'superadmin'){
             $data['patient'] = User::where('role','patient')->count();
             $data['hospital'] = User::where('role','admin')->count();
-            $data['appointments'] = Appointment::where('role','patient')->count();
+            $data['appointments'] =DB::select("select count(appointments.id) from appointments inner join users on users.id = appointments.user_id
+            where users.role = 'patient' and appointments.hospital_id = ".Auth::user()->hospital_id);
         }else{
             $data['patient'] = User::where('role','patient')->where('hospital_id',Auth::user()->hospital_id)->count();
-            $data['appointments'] = Appointment::where('hospital_id',Auth::user()->hospital_id)->where('role','patient')->count();
+            $data['appointments'] = Appointment::where('hospital_id',Auth::user()->hospital_id)->where('users.role','patient')->count();
         }
         return view('pages.dashboard')->with($data, 'data');
     }
